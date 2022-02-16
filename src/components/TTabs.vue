a434d743bccc303a3d7e264054b6b0fa62e46f5a
gau94rav <gau94rav@gmail.com>
2022-02-16 17:45:02 +0530

    refactor: simplified syntax for t-tabs

--- src/components/TTabs.vue
+++ src/components/TTabs.vue
@@ -1,9 +1,41 @@
 <template>
   <div
-    class="flex relative flex-wrap h-max"
-    :class="[isElevated && 'shadow', styles.bgColors[innerBgColor], isSidebar ? 'w-min' : 'w-full']"
+    class="h-max my-2 flex w-full shadow rounded overflow-x-hidden"
+    :class="isVertical ? 'flex-row' : 'flex-col'"
   >
-    <slot></slot>
+    <!-- Tab Buttons -->
+    <div class="flex" :class="[styles.bgColors[tabsBgColor], isVertical ? 'flex-col w-max' : 'w-full']">
+      <div
+        v-for="(child, index) in childNodes"
+        :key="index"
+        class="p-3 whitespace-nowrap transition-colors cursor-pointer bg-opacity-95 hover:bg-opacity-100"
+        :class="[
+          styles.bgColors[tabsBgColor],
+          styles.borderWidthSizes[linePosition][lineSize],
+          styles.fontSizes[titleSize],
+          isVertical ? 'w-full' : 'w-max',
+          activeIndex === index
+            ? [
+                'text-opacity-100',
+                styles.borders[lineStyle],
+                styles.borderColors[lineColor],
+                styles.textColors[titleHighlightColor],
+              ]
+            : ['text-opacity-90', 'border-transparent', styles.textColors[titleColor]],
+        ]"
+        @click="setActive(index)"
+      >
+        {{ child.title }}
+      </div>
+    </div>
+
+    <div
+      ref="slotsContainer"
+      class="py-4 px-2 bg-opacity-90 flex"
+      :class="[styles.bgColors[contentBgColor], styles.textColors[contentTextColor]]"
+    >
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -13,25 +45,62 @@ export default {
   name: 't-tabs',
   props: {
     // Strings
-    innerBgColor: {
+    lineColor: {
+      type: String,
+      default: 'neutral',
+      validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
+    },
+    lineStyle: {
+      type: String,
+      default: 'solid',
+      validator: (v) => ['solid', 'dashed', 'dotted', 'double'].includes(v),
+    },
+    lineSize: {
+      type: String,
+      default: 'medium',
+      validator: (v) => ['normal', 'medium', 'bold', 'extrabold'].includes(v),
+    },
+    contentBgColor: {
+      type: String,
+      default: 'transparent',
+      validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
+    },
+    contentTextColor: {
+      type: String,
+      default: 'dark',
+      validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
+    },
+    tabsBgColor: {
       type: String,
       default: 'light',
       validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
     },
-    targetId: {
+    titleColor: {
       type: String,
-      default: '',
+      default: 'dark',
+      validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
+    },
+    titleHighlightColor: {
+      type: String,
+      default: 'dark',
+      validator: (v) => ['primary', 'secondary', 'light', 'dark', 'neutral'].includes(v),
+    },
+    titleSize: {
+      type: String,
+      default: 'sm',
+      validator: (v) => ['sm', 'md', 'lg', 'xl', '2xl', '3xl'].includes(String(v)),
     },
 
     // Booleans
-    isElevated: {
+    isVertical: {
       type: Boolean,
       default: false,
     },
   },
   data() {
     return {
-      activeSection: '',
+      activeIndex: 0,
+      childNodes: [],
       styles: {
         bgColors: {
           primary: 'bg-primary-400',
@@ -39,29 +108,80 @@ export default {
           light: 'bg-neutral-50',
           dark: 'bg-neutral-900',
           neutral: 'bg-neutral-400',
+          transparent: 'bg-transparent',
+        },
+        borders: {
+          solid: 'border-solid',
+          dashed: 'border-dashed',
+          dotted: 'border-dotted',
+          double: 'border-double',
+        },
+        borderColors: {
+          primary: 'border-primary-400',
+          secondary: 'border-secondary-400',
+          light: 'border-neutral-50',
+          dark: 'border-neutral-900',
+          neutral: 'border-neutral-400',
+        },
+        borderWidthSizes: {
+          bottom: {
+            normal: 'border-b',
+            medium: 'border-b-2',
+            bold: 'border-b-4',
+            extrabold: 'border-b-8',
+          },
+          right: {
+            normal: 'border-r',
+            medium: 'border-r-2',
+            bold: 'border-r-4',
+            extrabold: 'border-r-8',
+          },
+        },
+        fontSizes: {
+          sm: 'text-sm',
+          md: 'text-md',
+          lg: 'text-lg',
+          xl: 'text-xl',
+          '2xl': 'text-2xl',
+          '3xl': 'text-3xl',
+        },
+        textColors: {
+          primary: 'text-primary-400',
+          secondary: 'text-secondary-400',
+          light: 'text-neutral-50',
+          dark: 'text-neutral-900',
+          neutral: 'text-neutral-400',
         },
       },
     }
   },
   computed: {
-    isSidebar() {
-      // Parent wrapper is forcing items to be in line.
-      // In that case convert the tabs to sidebar buttons.
-      return this.$parent.hasRows
+    linePosition() {
+      return this.isVertical ? 'right' : 'bottom'
     },
   },
+  mounted() {
+    this.getSlotElements()
+    this.setActive(0)
+  },
   methods: {
-    setActive(section) {
-      this.activeSection = section
-      this.hideSections()
+    getSlotElements() {
+      const slotContainer = this.$refs.slotsContainer
+      if (slotContainer) {
+        this.childNodes = slotContainer.children
+      }
     },
-    hideSections() {
-      const targetSection = document.getElementById(this.targetId)
-      if (targetSection) {
-        const childSections = targetSection.querySelectorAll('.t-tab-section')
-        if (childSections) {
-          for (let section of childSections) {
-            section.style.display = 'none'
+    setActive(index) {
+      this.activeIndex = index
+      for (let elIndex in this.childNodes) {
+        elIndex = parseInt(elIndex) // elIndex is type of string by default
+        const element = this.childNodes[elIndex]
+
+        if (typeof element === 'object') {
+          if (elIndex === index) {
+            element.style.display = 'inherit'
+          } else {
+            element.style.display = 'none'
           }
         }
       }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,103 +1,23 @@
 <template>
   <div>
-    <!-- Note: need to make a library like page for all components -->
-    <h1 class="pb-6">Tabs</h1>
-    <div class="flex flex-wrap">
-      <h1 class="py-4 font-extrabold">Tab Section 1</h1>
-      <t-section border-color="secondary" is-outlined>
-        <t-tabs is-elevated target-id="first-nav">
-          <t-tab line-color="secondary" target-id="tab_dark_1"> Text Inputs </t-tab>
-          <t-tab line-color="secondary" target-id="tab_dark_2"> Select Inputs </t-tab>
-        </t-tabs>
+    <h1 class="pt-2">Tabs</h1>
+    <t-tabs
+      tabs-bg-color="dark"
+      title-color="light"
+      title-highlight-color="secondary"
+      line-color="secondary"
+      line-style="dashed"
+      content-bg-color="dark"
+      content-text-color="light"
+    >
+      <div title="Section 1">This is section 1</div>
+      <div title="Section 2">This is section 2</div>
+    </t-tabs>
 
-        <t-tab-sections section-group-id="first-nav">
-          <t-tab-section section-id="tab_dark_1" inner-bg-color="light">
-            <t-text-input label="Default" />
-            <t-text-input
-              label="Label"
-              is-inside-label
-              is-outlined
-              is-rounded
-              inner-bg-color="dark"
-              text-color="light"
-              placeholder="Placeholder"
-            />
-          </t-tab-section>
-          <t-tab-section section-id="tab_dark_2" inner-bg-color="light"
-            >No Select inputs in this branch</t-tab-section
-          >
-        </t-tab-sections>
-      </t-section>
-
-      <h1 class="py-4 font-extrabold">Tab Section 2</h1>
-      <t-section is-rounded is-outlined>
-        <t-tabs inner-bg-color="dark" is-elevated target-id="second-nav">
-          <t-tab
-            line-color="primary"
-            inner-bg-color="dark"
-            title-color="light"
-            title-highlight-color="secondary"
-            title-size="lg"
-            target-id="secondary_nav_1"
-          >
-            Big Tab 1
-          </t-tab>
-          <t-tab
-            line-color="primary"
-            inner-bg-color="dark"
-            title-color="light"
-            title-highlight-color="secondary"
-            title-size="lg"
-            target-id="secondary_nav_2"
-          >
-            Big Tab 2
-          </t-tab>
-        </t-tabs>
-
-        <t-tab-sections section-group-id="second-nav">
-          <t-tab-section section-id="secondary_nav_1" inner-bg-color="dark" text-color="light">
-            This is dark section 1
-          </t-tab-section>
-          <t-tab-section section-id="secondary_nav_2" inner-bg-color="dark" text-color="light"
-            >This is dark section 2</t-tab-section
-          >
-        </t-tab-sections>
-      </t-section>
-
-      <h1 class="py-4 font-extrabold">Tab Section 3 (Sidebar)</h1>
-      <t-section is-rounded is-outlined is-elevated has-rows>
-        <t-tabs inner-bg-color="dark" is-elevated target-id="third_nav">
-          <t-tab
-            line-color="light"
-            inner-bg-color="primary"
-            title-color="light"
-            title-highlight-color="secondary"
-            title-size="lg"
-            target-id="sidebar_1"
-          >
-            Sidebar Tab 1
-          </t-tab>
-          <t-tab
-            line-color="light"
-            inner-bg-color="primary"
-            title-color="light"
-            title-highlight-color="secondary"
-            title-size="lg"
-            target-id="sidebar_2"
-          >
-            Sidebar Tab 2
-          </t-tab>
-        </t-tabs>
-
-        <t-tab-sections section-group-id="third_nav">
-          <t-tab-section section-id="sidebar_1" inner-bg-color="secondary" text-color="dark">
-            This is sidebar section 1
-          </t-tab-section>
-          <t-tab-section section-id="sidebar_2" inner-bg-color="secondary" text-color="dark"
-            >This is sidebar section 2</t-tab-section
-          >
-        </t-tab-sections>
-      </t-section>
-    </div>
+    <h1 class="pt-2">Text Inputs</h1>
+    <t-tabs>
+      <t-text-input label="Label" title="Default" />
+      <t-text-input is-outlined title="Outlined" label="Label" />
+    </t-tabs>
   </div>
 </template>
