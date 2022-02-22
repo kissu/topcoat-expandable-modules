47fb6356de17f8a1eed11b0d3c3fc3c057239502
Konstantin BIFERT <hello@kissu.io>
2022-02-22 11:57:09 +0100

    feature: export index components to individual pages

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,68 +1,17 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center space-y-2">
     <!-- Note: need to make a library like page for all components -->
-    <h1 class="pb-6">Text Inputs</h1>
-    <div class="grid grid-cols-3 gap-2 pb-6">
-      <t-text-input
-        label="Default"
-        border-width="medium"
-        border-color="dark"
-        border-highlight-color="secondary"
-      />
-      <t-text-input label="Default Outlined & Loading" is-outlined is-loading />
-      <t-text-input label="Outlined & Clearable" is-outlined is-clearable="true" />
-      <t-text-input
-        label="Label"
-        placeholder="Placeholder & Rounded 3xl"
-        border-width="bold"
-        border-color="primary"
-        border-highlight-color="secondary"
-        is-rounded="3xl"
-        is-outlined
-      />
-      <t-text-input
-        label="Label"
-        placeholder="Inside Label & dark color"
-        is-inside-label
-        is-rounded="3xl"
-        inner-bg-color="dark"
-        is-outlined
-        text-color="light"
-      />
-      <t-text-input
-        label="Label"
-        placeholder="Border width is bolder"
-        border-width="extrabold"
-        is-rounded="3xl"
-        is-outlined
-      />
-    </div>
-
-    <h1 class="pb-6">Select Inputs</h1>
-    <div class="grid grid-cols-3 gap-2 pb-6">
-      <t-select-input label="Default" />
-      <t-select-input label="Default Outlined & Loading" is-outlined is-loading />
-      <t-select-input
-        label="Bold Border"
-        border-width="bold"
-        border-color="primary"
-        border-highlight-color="secondary"
-        is-rounded="3xl"
-        is-outlined
-      />
-      <t-select-input
-        label="Dark and ExtraBold"
-        border-width="extrabold"
-        border-color="dark"
-        border-highlight-color="secondary"
-        inner-bg-color="primary"
-        text-color="light"
-        is-rounded="3xl"
-        is-outlined
-        menu-highlight-color="secondary"
-        menu-text-highlight-color="dark"
-        is-inside-label
-      />
-    </div>
+    <router-link
+      class="inline-block rounded-md bg-success-400 p-4 text-neutral-800"
+      :to="{ name: 'text-input' }"
+    >
+      text input
+    </router-link>
+    <router-link
+      class="inline-block rounded-md bg-success-400 p-4 text-neutral-800"
+      :to="{ name: 'select-input' }"
+    >
+      select input
+    </router-link>
   </div>
 </template>
---
+++ src/pages/text-input.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="pb-6">Text Inputs</h1>
+  <div class="grid grid-cols-3 gap-2 pb-6">
+    <t-text-input
+      label="Default"
+      border-width="medium"
+      border-color="dark"
+      border-highlight-color="secondary"
+    />
+    <t-text-input label="Default Outlined & Loading" is-outlined is-loading />
+    <t-text-input label="Outlined & Clearable" is-outlined is-clearable="true" />
+    <t-text-input
+      label="Label"
+      placeholder="Placeholder & Rounded 3xl"
+      border-width="bold"
+      border-color="primary"
+      border-highlight-color="secondary"
+      is-rounded="3xl"
+      is-outlined
+    />
+    <t-text-input
+      label="Label"
+      placeholder="Inside Label & dark color"
+      is-inside-label
+      is-rounded="3xl"
+      inner-bg-color="dark"
+      is-outlined
+      text-color="light"
+    />
+    <t-text-input
+      label="Label"
+      placeholder="Border width is bolder"
+      border-width="extrabold"
+      is-rounded="3xl"
+      is-outlined
+    />
+  </div>
+</template>
